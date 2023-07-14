6c2c367a474e15c6cb706831b6278a7f87346cc3
nirifabien1234 <nirifabien@gmail.com>
2023-07-14 10:08:54 +0200

    mend

--- src/components/Headers/CardsHeaderReferral.js
+++ src/components/Headers/CardsHeaderReferral.js
@@ -1,11 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 // nodejs library to set properties for components
 import PropTypes from "prop-types";
 // reactstrap components
 import {
-  Breadcrumb,
-  BreadcrumbItem,
-  Button,
   Card,
   CardBody,
   CardTitle,
