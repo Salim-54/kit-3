a61854559c59cb9cb57f0dbbd35816c4cccbcfbc
nirifabien1234 <nirifabien@gmail.com>
2023-07-17 17:36:32 +0200

    mend

---
+++ src/views/pages/examples/Privacy.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import pdfFile from '../../../assets/privacy-policy.pdf';
+
+const PDFRedirect = () => {
+
+window.open(pdfFile, '_self')
+};
+
+export default PDFRedirect;
